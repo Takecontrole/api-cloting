4592f9720c6634b1f3d6b0de366bdde0ad59fdfe
Takecontrole <takecontrole2034@gmail.com>
2023-04-07 13:30:44 +0400

    change of some data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Order = require('../models/Order');
+const getCountryIso3 = require ("country-iso-2-to-3");
 // signup
 
 router.post('/signup', async(req, res)=> {
@@ -41,6 +42,34 @@ router.get('/', async(req, res)=> {
 })
 
 // get user orders
+
+router.get('/geography', async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+
+
 
 router.get('/:id/orders', async (req, res)=> {
   const {id} = req.params;
--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/create-payment', async(req, res)=> {
    }
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 server.listen(port, ()=> {
   console.log('server running at port', port)
 })
